294cec06cd3026611a33a9f56c4dce8f6b2823c5
YashM1234 <yashmungolerpg@gmail.com>
2023-04-01 02:48:16 +0530

    Assignment-1 on If-else Statement...

---
+++ src/main/java/com/bootcoding/java/flowcontrol/Assignment1EvenOddFinder.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.flowcontrol;
+//WAP to find the given number is even or odd.
+public class Assignment1EvenOddFinder {
+    public static void main(String[] args) {
+        int num1 = 10;
+        if(num1 % 2 == 0){
+            System.out.println("The given number is " + num1 + " and it is even.");
+        }
+        else{
+            System.out.println("The given number is " + num1 + " and it is odd.");
+        }
+    }
+}
