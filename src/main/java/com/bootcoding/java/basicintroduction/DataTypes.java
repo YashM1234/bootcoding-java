dd5239d01efee45cbf75f8b2fd6c15aa78368883
YashM1234 <yashmungolerpg@gmail.com>
2023-04-01 00:22:28 +0530

    Java Primitive Data Type...

---
+++ src/main/java/com/bootcoding/java/basicintroduction/DataTypes.java
@@ -0,0 +1,33 @@
+package com.bootcoding.java.basicintroduction;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //data types specify the type of data that can be stored inside variables in Java.
+        //In Java there are 8 primitive data types
+        //1. boolean
+        boolean isTrue = true;
+        System.out.println(isTrue);
+        //2. byte
+        byte b = 127;
+        System.out.println(b);
+        //3. short
+        short s = 32767;
+        System.out.println(s);
+        //4. int
+        int i = 23100000;
+        System.out.println(i);
+        //5. long
+        long l = 26300000L;
+        System.out.println(l);
+        //6. float
+        float f = 56.25f;
+        System.out.println(f);
+        //7. double
+        double d = 105.339;
+        System.out.println(d);
+        //8. char
+        char c = 'A';
+        System.out.println(c);
+
+    }
+}
