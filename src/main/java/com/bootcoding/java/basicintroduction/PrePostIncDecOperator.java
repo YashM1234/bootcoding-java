7d7374365068148364cdaba7f5b5961ac0cea3e7
YashM1234 <yashmungolerpg@gmail.com>
2023-04-01 01:44:04 +0530

    Java Pre-Increment/Decrement & Post-Increment/Decrement Operators...

--- src/main/java/com/bootcoding/java/basicintroduction/PrePostIncDecOperator.java
+++ src/main/java/com/bootcoding/java/basicintroduction/PrePostIncDecOperator.java
@@ -7,16 +7,16 @@ public class PrePostIncDecOperator {
         System.out.println("PRE-INCREMENT BY ONE");
         int preIncrement = 5;
         System.out.println("Initial Value: " + preIncrement );
-        int result2 = 10 + ++preIncrement;
-        System.out.println("result (10 + ++preIncrement) = " + result2);
+        int result1 = 10 + ++preIncrement;
+        System.out.println("result (10 + ++preIncrement) = " + result1);
         System.out.println();
 
         //post-Increment -->First read then Increment
         System.out.println("POST-INCREMENT BY ONE");
         int postIncrement = 5;
         System.out.println("Initial Value: " + postIncrement );
-        int result1 = 10 + postIncrement++;
-        System.out.println("result (10 + postIncrement++) = " + result1);
+        int result2 = 10 + postIncrement++;
+        System.out.println("result (10 + postIncrement++) = " + result2);
         System.out.println("Now it increase by 1: " + postIncrement);
         System.out.println();
 
@@ -24,16 +24,16 @@ public class PrePostIncDecOperator {
         System.out.println("PRE-DECREMENT BY ONE");
         int preDecrement = 5;
         System.out.println("Initial Value: " + preDecrement );
-        int result4 = 10 + --preDecrement;
-        System.out.println("result (10 + --preDecrement) = " + result4);
+        int result3 = 10 + --preDecrement;
+        System.out.println("result (10 + --preDecrement) = " + result3);
         System.out.println();
 
         //post-Decrement -->First read then Decrement
         System.out.println("POST-DECREMENT BY ONE");
         int postDecrement = 5;
         System.out.println("Initial Value: " + postDecrement );
-        int result3 = 10 + postDecrement--;
-        System.out.println("result (10 + postDecrement--) = " + result3);
+        int result4 = 10 + postDecrement--;
+        System.out.println("result (10 + postDecrement--) = " + result4);
         System.out.println("Now it decrease by 1: " + postDecrement);
 
     }
