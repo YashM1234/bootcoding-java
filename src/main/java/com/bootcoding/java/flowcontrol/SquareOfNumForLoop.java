6e06eebbd88fd616eda2a746a976e854501128b6
YashM1234 <yashmungolerpg@gmail.com>
2023-04-03 14:55:32 +0530

    Print square of each number from 1 to given number...

---
+++ src/main/java/com/bootcoding/java/flowcontrol/SquareOfNumForLoop.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.flowcontrol;
+//WAP to print square of each number from 1 to given number
+public class SquareOfNumForLoop {
+    public static void main(String[] args) {
+        int num = 20;
+        for(int i = 1; i <= num; i++){
+            System.out.println(i*i);
+        }
+    }
+}
