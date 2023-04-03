a16e67af099ffa77944eafd7b39f73c4d47dbc20
YashM1234 <yashmungolerpg@gmail.com>
2023-04-03 14:52:43 +0530

    Basic of for loop...

---
+++ src/main/java/com/bootcoding/java/flowcontrol/ForLoop.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.flowcontrol;
+//for loop in java
+public class ForLoop {
+    public static void main(String[] args) {
+        /* Syntax:
+                for (initialExpression; testExpression; updateExpression) {
+                    //Body of loop
+                }
+         */
+        //WAP to print positive numbers from 1 to 10
+        for(int i = 1; i <=10; i++){
+            System.out.println(i);
+        }
+    }
+}
