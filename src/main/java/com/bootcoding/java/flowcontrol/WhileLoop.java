eb3f367c9eb14c639cdf1a96698f9517ff71f14c
YashM1234 <yashmungolerpg@gmail.com>
2023-04-04 17:06:21 +0530

    Basic of while Loop in Java...

---
+++ src/main/java/com/bootcoding/java/flowcontrol/WhileLoop.java
@@ -0,0 +1,18 @@
+package com.bootcoding.java.flowcontrol;
+//While Loop in Java
+public class WhileLoop {
+    public static void main(String[] args) {
+        /*
+        Syntax: while(condition is true){
+                    //body of loop
+                 }
+         */
+        //WAP to print numbers from 1 to given number
+        int i = 1;
+        int ui = 5;
+        while(i <= ui){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
