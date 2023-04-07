fd2933febaefe711c4e52b551025943d16d23507
YashM1234 <yashmungolerpg@gmail.com>
2023-04-07 21:47:37 +0530

    Print an odd element of an array...

---
+++ src/main/java/com/bootcoding/java/array/ArrayOddPrint.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.array;
+//WAP to print an odd element of an array [7, 22, 19, 44, 45, 65, 66]
+public class ArrayOddPrint {
+    public static void main(String[] args) {
+        int[] arr = {7, 22, 19, 44, 45, 65, 66};
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]%2 != 0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
