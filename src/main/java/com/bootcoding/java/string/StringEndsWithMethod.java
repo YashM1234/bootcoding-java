9b5347af472ba32245528e64b0680e5c6d17f290
YashM1234 <yashmungolerpg@gmail.com>
2023-04-21 22:18:34 +0530

    String equals() method...

---
+++ src/main/java/com/bootcoding/java/string/StringEndsWithMethod.java
@@ -0,0 +1,23 @@
+package com.bootcoding.java.string;
+//String endsWith() method
+public class StringEndsWithMethod {
+    public static void main(String[] args) {
+        /*
+        10. endsWith() method:
+            This method checks whether the string ends with the specified string or not.
+            Syntax:
+                string.endsWith(String str); //Here str is a string which we have to check with.
+            Argument:
+                It takes single argument i.e. str
+            Return value:
+                Returns true if string ends with given str, returns false if string doesn't end with given str.
+         */
+
+        //WAP to demonstrate endsWith() method
+        String str = "This is string which we have to check";
+        System.out.println(str.endsWith("check"));
+        System.out.println(str.endsWith("ck"));
+        System.out.println(str.endsWith("This"));
+        System.out.println(str.endsWith("is"));
+    }
+}
